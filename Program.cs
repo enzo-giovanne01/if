8ed33bb674a59f8a0ac5076d17e289ca5d3587ac
enzo-giovanne01 <giovanneenzo01@gmail.com>
2024-09-02 00:05:06 -0300

    if

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+
+int a = 8;
+if(a < 0)
+{
+    WriteLine($"O número {a} é negativo.");
+}
+else if(a > 0 && a < 10)
+{
+    WriteLine($"O número {a} esta entre 0 - 10.");
+}
+else
+{
+    WriteLine($"O número {a} é maior que 10.");
+}
